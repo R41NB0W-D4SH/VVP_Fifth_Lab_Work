d14a8bea06b22514668efb6d95569ba2ab6810b6
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-30 12:36:05 +0300

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	double a, b;
+
+	cout << "Введите размер файла в байтах" << endl;
+	cin >> a;
+	b = floor(a / 1024);
+	cout << "Размер файла в байтах равен " << b << endl;
+
+	system("pause");
+	return 0;
+}

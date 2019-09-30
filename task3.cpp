d70721b3297281601b6816da428f00ef9d3ed7c1
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-30 12:59:52 +0300

    Create task3.cpp

---
+++ task3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int a, b, c;
+
+	cout << "Введите числа A и B так, чтобы A было больше B и они были бы положительными " << endl;
+	cin >> a;
+	cin >> b;
+
+	if (a > 0 && b > 0)
+	{
+		if (a > b)
+		{
+			c = a / b;
+			a = a - b * c;
+			cout << "Длина незанятой части отрезка A равна " << a << endl;
+		}
+		else
+		{
+			cout << "Вы ввели число B, которое больше A, что не следует условиям задачи. Попробуйте еще раз " << endl;
+		}
+	}
+	else
+	{
+		cout << "Вы ввели отрицательные A и B. Попробуйте еще раз, но следуйте условию " << endl;
+	}
+
+	system("pause");
+	return 0;
+}
